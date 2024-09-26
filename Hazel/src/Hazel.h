7277dd5cf196eca16e7ca0238c0a6dab46be11b7
Duan-start <2082118355@qq.com>
2024-09-26 21:15:13 +0800

    Setup basic Application a

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//for used by hazel application
+
+#include "Hazel/Application.h"
+
+//---Entity Point-------------
+#include"Hazel/EntryPoint.h"
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"Core.h"
+
+namespace Hazel {
+class HAZEL_API Application {
+public:	
+	Application();
+	virtual ~Application();
+	void Run();
+};
+//在客户端将会被定义
+Application* CreateApplication();
+}
