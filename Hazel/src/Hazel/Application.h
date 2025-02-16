f14f93524692cbf61c36f919bb0c814d52675f6c
Duan-start <2082118355@qq.com>
2025-02-16 18:21:57 +0800

    ImGui

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -17,12 +17,16 @@ public:
 	void OnEvent(Event& e);
 	void PushLayer(Layer* layer);
 	void PushOverlay(Layer* overlay);
+	inline static Application& Get() { return *s_Instance; }
+	inline Window& GetWindow(){ return  *m_Window; }
 private:
 	bool OnWindowClose(WindowClose& e);
 	std::unique_ptr<Window> m_Window;
 	bool m_Running = true;
 	LayerStack m_LayerStack;
-	
+
+private:
+	static Application* s_Instance;
 };
 //在客户端将会被定义
 Application* CreateApplication();
