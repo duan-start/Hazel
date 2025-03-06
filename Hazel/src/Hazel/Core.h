ef590e83ca30b4d971736c86c1a9a29aed50a896
Duan-start <2082118355@qq.com>
2025-03-06 22:58:25 +0800

    静态库

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
+#ifdef HZ_DYNAMIC_LINK
     #ifdef HZ_BUILD_DLL
         #define HAZEL_API _declspec(dllexport)
    #else
         #define HAZEL_API _declspec(dllimport)
       #endif
+#else 
+#define HAZEL_API
+#endif
+
 #else
        #error Hazel only support Windows !
 #endif
