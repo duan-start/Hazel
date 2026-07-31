550986de303062326d183d3a64ec6b56ac369c23
DHJ\段宏进 <2082119355@qq.com>
2026-07-31 09:56:50 +0800

    LinkMono

--- Dependencies.lua
+++ Dependencies.lua
@@ -13,6 +13,7 @@ IncludeDir["yaml_cpp"] = "%{wks.location}/Hazel/vendor/yaml-cpp/include"
 IncludeDir["Box2D"] = "%{wks.location}/Hazel/vendor/Box2D/include"
 IncludeDir["GLFW"] = "%{wks.location}/Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "%{wks.location}/Hazel/vendor/Glad/include"
+IncludeDir["mono"] = "%{wks.location}/Hazel/vendor/mono/include"
 IncludeDir["ImGui"] = "%{wks.location}/Hazel/vendor/imgui"
 IncludeDir["ImGuizmo"] = "%{wks.location}/Hazel/vendor/ImGuizmo"
 IncludeDir["glm"] = "%{wks.location}/Hazel/vendor/glm"
@@ -21,24 +22,26 @@ IncludeDir["shaderc"] = "%{wks.location}/Hazel/vendor/shaderc/include"
 IncludeDir["SPIRV_Cross"] = "%{wks.location}/Hazel/vendor/SPIRV-Cross"
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 
+
 --Initialized Lidrary,指定原始的lib和编译到本地项目的debug版本
 LibraryDir = {}
-
 LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
 LibraryDir["VulkanSDK_Debug"] = "%{wks.location}/Hazel/vendor/VulkanSDK/Lib"
-LibraryDir["assimp"] = "%{wks.location}/Hazel/vendor/assimp/X64"
+LibraryDir["assimp"] = "%{wks.location}/Hazel/vendor/assimp"
+LibraryDir["mono"] = "%{wks.location}/Hazel/vendor/mono/%{cfg.buildcfg}/lib"
 --利用上面已经定义好的去找到对应的库
 Library = {}
-Library["Assimp"]="%{LibraryDir.assimp}/assimp-vc143-mtd.lib"
-
+Library["Mono"] = "%{LibraryDir.mono}/mono-2.0-sgen.lib"
+Library["Assimp"]="%{LibraryDir.assimp}/Libs/assimp-vc143-mtd.lib"
 Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
 Library["VulkanUtils"] = "%{LibraryDir.VulkanSDK}/VkLayer_utils.lib"
 
 Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_sharedd.lib"
+Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
+
 Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-cored.lib"
 Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsld.lib"
 Library["SPIRV_Tools_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/SPIRV-Toolsd.lib"
 
-Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
 Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
 Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
